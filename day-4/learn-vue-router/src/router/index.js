dee2a8a81aaff98a818710a01621c5c7f7d12685
GraceWalk <gracewalk1999@163.com>
2020-02-12 16:16:29 +0800

    路由懒加载写法
    Signed-off-by: GraceWalk <gracewalk1999@163.com>

--- day-4/learn-vue-router/src/router/index.js
+++ day-4/learn-vue-router/src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from '../components/Home'
-import About from '../components/About'
-import User from '../components/User'
+// import Home from '../components/Home'
+// import About from '../components/About'
+// import User from '../components/User'
+const Home = () => import('../components/Home')
+const About = () => import('../components/About')
+const User = () => import('../components/User')
 Vue.use(Router)
 
 const routes = [
