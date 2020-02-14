841d43517e795c0605dbf83cd01c2bed8b5dd81b
GraceWalk <gracewalk1999@163.com>
2020-02-14 10:31:31 +0800

    complete packaging the TabBar component
    Signed-off-by: GraceWalk <gracewalk1999@163.com>

--- day-6/tabbar/src/components/tabbar/TabBar.vue
+++ day-6/tabbar/src/components/tabbar/TabBar.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="tab-bar">
-    <div class="tab-bar-item">首页</div>
-    <div class="tab-bar-item">分页</div>
-    <div class="tab-bar-item">购物车</div>
-    <div class="tab-bar-item">我的</div>
+    <slot></slot>
   </div>
 </template>
 
@@ -22,10 +19,5 @@
     bottom: 0;
     background-color: rgb(231, 231, 231);
     box-shadow: 0 -1px 3px rgba(204, 204, 204, 0.747)
-  }
-  .tab-bar-item {
-    flex: 1;
-    height: 49px;
-    text-align: center;
   }
 </style>
